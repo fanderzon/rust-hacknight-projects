cf1b99d859bc26df52f78f2bb9c59b1a6b82ba30
Fredrik Andersson <f.anderzon@gmail.com>
2017-01-26 15:50:33 +0100

    Added terminal todo list task

---
+++ terminal-todo/README.md
@@ -0,0 +1,50 @@
+# CLI Todo list
+Your job is to create a Todo list that runs in the terminal.
+
+When you build the project with `cargo run` it should print the current Todo list (which is probably empty on init).
+
+The use should be able to use the Todo list in the following way:
+
+## Adding todos
+
+`add some kind of todo item`
+
+Should add "some kind of todo item" as a todo item in the list, and automatically assign it an id.
+
+After every command the tool should print out the current todo list, which should now look something like this:
+
+```
+Todo List:  
+-------------------
+[ ] 1 some kind of todo item
+```
+
+## Marking todos as done
+
+`done 1`
+
+Should mark the todo item with id 1 as done.
+
+```
+Todo List:  
+-------------------
+[✔] 1 some kind of todo item
+```
+
+## Removing todos
+
+`remove 1`
+
+Should remove the todo item with id 1.
+
+```
+Todo List:  
+-------------------
+```
+
+
+# Additional ideas
+If this wasn't challenging enough here are some other features to implement:
+* Filtering todos to show only checked, or unchecked items
+* Persist todo list to file or database
+* Handling multiple todo lists `load list shopping`?
--- terminal-todo/src/main.rs
+++ terminal-todo/src/main.rs
@@ -1,3 +1,44 @@
+use std::io; // Import needed to receive input from the command line
+
+// Struct to represent a todo item (feel free to edit as needed)
+struct Todo {
+    id: i16,
+    title: String,
+    completed: bool,
+    deleted: bool,
+}
+
+// Helper function to print the todo list
+fn print_todos(todos: &Vec<Todo>) {
+    println!("\n\nTodo List:\n-------------------");
+    for todo in todos {
+        if !todo.deleted {
+            let done = if todo.completed { "✔" } else { " " };
+            println!("[{}] {} {}", done, todo.id, todo.title);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Empty vector/List to hold your todos
+    let mut todos: Vec<Todo> = Vec::new();
+    print_todos(&todos);
+
+    loop {
+        let mut command = String::new();
+        io::stdin()
+            .read_line(&mut command)
+            .expect("failed to read line");
+
+        // text input split by spaces into a vector, so if command is:
+        // `add buy milk`
+        // command_parts[0] will be "add", command_parts[1] will be "buy", and so on.
+        // Feel free to parse input in any way you want, this is to help you get started
+        let command_parts: Vec<&str> = command.split_whitespace().collect();
+
+        // Recognize commands and do something with the `todos` vector here
+
+        // At the end of each loop print the list
+        print_todos(&todos);
+    }
 }
